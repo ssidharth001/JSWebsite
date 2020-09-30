4a8b2446bd0e055d1499e838276e84c88b35720f
Sidharth S <sidharths@qburst.com>
2020-09-30 08:24:00 +0530

    Added contact form

--- js/main.js
+++ js/main.js
@@ -1,16 +1,16 @@
 var xhttp = new XMLHttpRequest();
-xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      var response = JSON.parse(this.responseText);
-      
-      //----------------------------for table headers--------------------------------
-      var tableheader = response.tableheader;
-      var output3 = '';
-      for(var i = 0;i < tableheader.length; i++){
-        output3 += '<th>'+ tableheader[i].header +'</th>'
-      };
-      document.getElementById("table-head").innerHTML = output3;    
-    };
+xhttp.onreadystatechange = function () {
+  if (this.readyState == 4 && this.status == 200) {
+    var response = JSON.parse(this.responseText);
+
+    //----------------------------for table headers--------------------------------
+    var tableheader = response.tableheader;
+    var output3 = "";
+    for (var i = 0; i < tableheader.length; i++) {
+      output3 += "<th>" + tableheader[i].header + "</th>";
+    }
+    document.getElementById("table-head").innerHTML = output3;
+  }
 };
-xhttp.open("GET", "json/menu.json", true);
+xhttp.open("GET", "json/main.json", true);
 xhttp.send();
