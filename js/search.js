f34cc4f5e9ebe9247b57f1e3409dfb74908bfa7b
ssidharth001 <sidharths@qburst.com>
2020-10-05 00:55:25 +0530

    Made page responsive

--- js/search.js
+++ js/search.js
@@ -1,85 +1,29 @@
-// document.getElementById("search").addEventListener("keyup", () => {
-//   var search = document.getElementById("search").value;
-//   var pText = document.getElementsByTagName("p");
-//   for (var values of pText) {
-//     values.innerHTML;
-//   }
-// });
-
 document.addEventListener(
   "DOMContentLoaded",
   function () {
     var originalContent = "";
-    var searchContents = "";
-    document.getElementById("search").addEventListener("keydown", function (e) {
-      if (searchContents == "") {
-        searchContents = document.getElementsByTagName("p");
+    document.getElementById("search").addEventListener("keyup", function () {
+      let searchContent = document.getElementById("first-para").innerHTML;
+      searchContent = searchContent.toString();
+      if (originalContent == "") {
+        originalContent = searchContent;
       }
-      if (e.key === "Enter") {
-        for (var values of searchContents) {
-          searchedTag = values;
-          searchContent = values.innerHTML;
-
-          searchContent = searchContent.toString();
-
-          originalContent = searchContent;
-
-          highlightWord(originalContent);
-        }
-        searchContent = "";
-      }
+      highlightWord(originalContent);
     });
   },
   false
 );
 
 function highlightWord(searchContent) {
-  var text = document.getElementById("search").value;
-  if (text) {
-    let newsearch = searchContent;
-    var pattern = new RegExp("(" + text + ")", "g");
-    var new_text = newsearch.replace(
+  var searchTxt = document.getElementById("search").value;
+  if (searchTxt) {
+    var pattern = new RegExp("(" + searchTxt + ")", "g");
+    var highlightedTxt = searchContent.replace(
       pattern,
-      "<span class='highlight'>" + text + "</span>"
+      "<span class='highlight'>" + searchTxt + "</span>"
     );
-    searchedTag.innerHTML = new_text;
+    document.getElementById("first-para").innerHTML = highlightedTxt;
   } else {
-    console.log(searchContent);
-    searchedTag.innerHTML = searchContent;
+    document.getElementById("first-para").innerHTML = searchContent;
   }
 }
-
-// document.addEventListener(
-//   "DOMContentLoaded",
-//   function () {
-//     var originalContent = "";
-//     document.getElementById("search").addEventListener("keydown", function (e) {
-//       let searchContent = document.getElementById("first-para").innerHTML;
-//       searchContent = searchContent.toString();
-//       if (originalContent == "") {
-//         originalContent = searchContent;
-//       }
-//       console.log(originalContent);
-//       if (e.key === "Enter") {
-//         console.log(originalContent);
-//         highlightWord(originalContent);
-//       }
-//     });
-//   },
-//   false
-// );
-
-// function highlightWord(searchContent) {
-//   var text = document.getElementById("search").value;
-//   if (text) {
-//     let newsearch = searchContent;
-//     var pattern = new RegExp("(" + text + ")", "g");
-//     var new_text = newsearch.replace(
-//       pattern,
-//       "<span class='highlight'>" + text + "</span>"
-//     );
-//     document.getElementById("first-para").innerHTML = new_text;
-//   } else {
-//     document.getElementById("first-para").innerHTML = searchContent;
-//   }
-// }
